2a54be0c69bcdc3e83150f52f1aeec20581d8287
Jason Raimondi <jason@raimondi.us>
2017-12-14 14:49:05 -0800

    Revert "Wip."
    This reverts commit c569cbb9aad0f9b8e6b5541f169629b696606f1c.

--- src/angular/Memories/memory.service.ts
+++ src/angular/Memories/memory.service.ts
@@ -1,44 +1,24 @@
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {AngularFireDatabase} from 'angularfire2/database';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
 
-export class MemoryEvent {
-    constructor(public text?: 'Karaoke'|'WeirdFact'|'CheckIn'|string,
-                public title?: string,
-                public timestamp?: number) {
-    }
-
-    get isKaraoke(): boolean {
-        return this.text === 'Karaoke';
-    }
-
-    get isWeirdFact(): boolean {
-        return this.text === 'WeirdFact';
-    }
-
-    get isCheckIn(): boolean {
-        return this.text === 'CheckIn';
-    }
+export class Memory {
 }
 
 @Injectable()
 export class MemoryService {
-    readonly memoryPath = '7fff5387-0000-45a0-a38a-5c260d22d3fb/digitalMemoryBank';
+  readonly memoryPath = '7fff5387-0000-45a0-a38a-5c260d22d3fb/digitalMemoryBank';
 
-    constructor(private angularFire: AngularFireDatabase) {
-    }
+  constructor(private angularFire: AngularFireDatabase) {
+  }
 
-    getEvents(id: string): Observable<MemoryEvent> {
-        return this.angularFire.list(this.memoryPath + '/' + id + '/events').valueChanges().map((event: any) => {
-            return new MemoryEvent(event.text, event.title, event.title);
-        });
-    }
+  getEvents(id: string): Observable<any> {
+    return this.angularFire.list(this.memoryPath + '/' + id + '/events').valueChanges();
+  }
 
-    getDigitalMemoryBank(id: string): Observable<any> {
-        return this.angularFire
-            .object(this.memoryPath + '/' + id)
-            .valueChanges();
-    }
+  getMemory(id: string): Observable<Memory> {
+    return this.angularFire.object(this.memoryPath + '/' + id).valueChanges();
+  }
 }
--- src/angular/Memories/memory/memory.component.ts
+++ src/angular/Memories/memory/memory.component.ts
@@ -2,7 +2,7 @@ import 'rxjs/add/operator/switchMap';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
-import { MemoryEvent, MemoryService } from '../memory.service';
+import { Memory, MemoryService } from '../memory.service';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -11,12 +11,7 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class MemoryDetailComponent implements OnInit {
-    readonly checkedInIcon = require('../../assets/checked-in.svg');
-    readonly funFactCorrectIcon = require('../../assets/fun-fact-correct.svg');
-    readonly karaokeIcon = require('../../assets/karaoke.svg');
-    readonly rightPersonIcon = require('../../assets/pts-for-being-the-right-person.svg');
-
-    private _memory$: Observable<MemoryEvent>;
+    private _memory$: Observable<Memory>;
     private _events$: Observable<any>;
 
     constructor(
@@ -25,30 +20,18 @@ export class MemoryDetailComponent implements OnInit {
         private service: MemoryService
     ) {
         this._memory$ = this.route.paramMap
-            .switchMap((params: ParamMap) => this.service.getDigitalMemoryBank(params.get('id')));
+            .switchMap((params: ParamMap) => this.service.getMemory(params.get('id')));
 
         this._events$ = this.route.paramMap
             .switchMap((params: ParamMap) => this.service.getEvents(params.get('id')));
     }
 
-    get memory$(): Observable<MemoryEvent> {
+    get memory$(): Observable<Memory> {
         return this._memory$;
     }
 
-    get events$(): Observable<MemoryEvent> {
+    get events$(): Observable<Memory> {
         return this._events$;
-    }
-
-    public getIconForMemory(event: MemoryEvent): any {
-        if (event.isKaraoke) {
-            return this.karaokeIcon;
-        } else if (event.isCheckIn) {
-            return this.checkedInIcon;
-        } else if (event.isWeirdFact) {
-            return this.funFactCorrectIcon;
-        }
-
-        return this.rightPersonIcon;
     }
 
     ngOnInit() {
