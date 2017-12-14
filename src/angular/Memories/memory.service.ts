eb52aa3a5d5a901d41c02f7f57ecb0e005ace12f
Jason Raimondi <jason@raimondi.us>
2017-12-14 14:49:30 -0800

    Revert "Revert "Wip.""
    This reverts commit 2a54be0c69bcdc3e83150f52f1aeec20581d8287.

--- src/angular/Memories/memory.service.ts
+++ src/angular/Memories/memory.service.ts
@@ -1,24 +1,44 @@
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { AngularFireDatabase } from 'angularfire2/database';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {AngularFireDatabase} from 'angularfire2/database';
 
-export class Memory {
+export class MemoryEvent {
+    constructor(public text?: 'Karaoke'|'WeirdFact'|'CheckIn'|string,
+                public title?: string,
+                public timestamp?: number) {
+    }
+
+    get isKaraoke(): boolean {
+        return this.text === 'Karaoke';
+    }
+
+    get isWeirdFact(): boolean {
+        return this.text === 'WeirdFact';
+    }
+
+    get isCheckIn(): boolean {
+        return this.text === 'CheckIn';
+    }
 }
 
 @Injectable()
 export class MemoryService {
-  readonly memoryPath = '7fff5387-0000-45a0-a38a-5c260d22d3fb/digitalMemoryBank';
+    readonly memoryPath = '7fff5387-0000-45a0-a38a-5c260d22d3fb/digitalMemoryBank';
 
-  constructor(private angularFire: AngularFireDatabase) {
-  }
+    constructor(private angularFire: AngularFireDatabase) {
+    }
 
-  getEvents(id: string): Observable<any> {
-    return this.angularFire.list(this.memoryPath + '/' + id + '/events').valueChanges();
-  }
+    getEvents(id: string): Observable<MemoryEvent> {
+        return this.angularFire.list(this.memoryPath + '/' + id + '/events').valueChanges().map((event: any) => {
+            return new MemoryEvent(event.text, event.title, event.title);
+        });
+    }
 
-  getMemory(id: string): Observable<Memory> {
-    return this.angularFire.object(this.memoryPath + '/' + id).valueChanges();
-  }
+    getDigitalMemoryBank(id: string): Observable<any> {
+        return this.angularFire
+            .object(this.memoryPath + '/' + id)
+            .valueChanges();
+    }
 }
